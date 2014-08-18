d493bf65acb4a84db82db45b53f87704a9e4c750
darrenmwong <darrenwong06@gmail.com>
2014-08-17 22:00:57 -0700

    about.html template

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 - Yeoman
 - Express.js
 
-Test
+  
--- packages/system/public/routes/system.js
+++ packages/system/public/routes/system.js
@@ -11,7 +11,14 @@ angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
       .state('home', {
         url: '/',
         templateUrl: 'system/views/index.html'
+      }),
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'system/views/about.html'
       });
+
+
   }
 ]).config(['$locationProvider',
   function($locationProvider) {
